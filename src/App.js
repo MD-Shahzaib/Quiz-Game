844abfa552bf718fa272d5b7d5c1b88e48a6a075
Muhammad Shahzaib <mdshahzaib36@gmail.com>
2023-07-20 01:14:27 -0700

    Create UI for quiz game

--- src/App.js
+++ src/App.js
@@ -1,11 +1,110 @@
-import React from 'react';
-import Quiz from './Quiz';
+import React, { useEffect, useState } from 'react';
+import Questions from './components/data/questions.json'
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
+  const [questions, setQuestions] = useState([]);
+  useEffect(() => {
+    setQuestions(Questions)
+  }, [])
+
+  // TIMER FUNCTION.
+  const [timer, setTimer] = useState(1);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimer((prevTimer) => prevTimer + 1);
+    }, 1000);
+    return () => { clearInterval(interval) };
+  }, []);
+
+  // CLOCK FUNCTION FOR (Vanila-JS)
+  /*
+  function startClock(displayElement) {
+    function updateClock() {
+      const now = new Date();
+      const hours = now.getHours().toString().padStart(2, '0');
+      const minutes = now.getMinutes().toString().padStart(2, '0');
+      const seconds = now.getSeconds().toString().padStart(2, '0');
+      const time = `${hours}:${minutes}:${seconds}`;
+      displayElement.textContent = time;
+    }
+    // Update the clock immediately
+    updateClock();
+    // Update the clock every second
+    setInterval(updateClock, 1000);
+  }
+  // Example usage:
+  const displayElement = document.getElementById("clock"); // Element to display the clock
+  startClock(displayElement);
+  */
+
+  // CLOCK FUNCTION FOR (REACT)
+  /*
+  const [time, setTime] = useState('');
+  useEffect(() => {
+    const updateClock = () => {
+      const now = new Date();
+      const hours = now.getHours().toString().padStart(2, '0');
+      const minutes = now.getMinutes().toString().padStart(2, '0');
+      const seconds = now.getSeconds().toString().padStart(2, '0');
+      const currentTime = `${hours}:${minutes}:${seconds}`;
+      setTime(currentTime);
+    };
+    // Update the clock immediately
+    updateClock();
+    // Update the clock every second
+    const intervalId = setInterval(updateClock, 1000);
+    // Clean up the interval on component unmount
+    return () => clearInterval(intervalId);
+  }, []);
+  */
+
   return (
-    <div>
-      <Quiz />
-    </div>
+    <>
+      <Navbar />
+      <div className="bg-slate-600">
+        <div className="container mx-auto p-4">
+          <h1 className='text-gray-200 text-4xl mb-4'>JavaScript Quiz</h1>
+          <h3 className='text-gray-200 text-xl mb-4 flex justify-between items-center'>
+            <span>Question 1 of {questions.length}</span>
+            <span>Time remaining: {timer} seconds</span>
+          </h3>
+          {questions?.map((item) => {
+            return (
+              <div key={item.id} className='bg-slate-200 p-2 my-2 rounded-md'>
+                <h2 className='text-2xl' >{parseInt(item.id) + 1} - {item.question}</h2>
+                <ul className='my-5'>
+                  {item.options.map((opt, index) => {
+                    return (
+                      <label
+                        key={index}
+                        htmlFor={opt}
+                        className="flex items-center space-x-2 p-2 bg-slate-700 text-white my-2 rounded-xl"
+                      >
+                        <input
+                          type="radio"
+                          name="option"
+                          id={opt}
+                          value={opt}
+                          className="form-radio h-5 w-5 text-blue-500"
+                        />
+                        <span>{opt}</span>
+                      </label>
+                    )
+                  })}
+                </ul>
+                <div className="flex justify-between items-center">
+                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">&larr; Previous</button>
+                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Next &rarr;</button>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 }
 
--- src/components/ExtraCode.js
+++ src/components/ExtraCode.js
@@ -193,3 +193,141 @@ const Timer = ({ time, onTimeout }) => {
 
 export default Timer;
 */
+
+
+
+// QUIZ.JS
+/*
+import React, { useEffect, useState } from 'react';
+
+const Quiz = () => {
+    const [questions, setQuestions] = useState([]);
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [score, setScore] = useState(0);
+    const [selectedAnswer, setSelectedAnswer] = useState(null);
+    const [timer, setTimer] = useState(10);
+
+    useEffect(() => {
+        fetchQuestions();
+    }, []);
+
+    const fetchQuestions = async () => {
+        try {
+            const response = await fetch(
+                'https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple'
+            );
+            const data = await response.json();
+            setQuestions(data.results);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        const countdown = setInterval(() => {
+            setTimer((prevTimer) => prevTimer - 1);
+        }, 1000);
+
+        if (timer === 0) {
+            handleAnswer(null);
+        }
+
+        return () => clearInterval(countdown);
+    }, [timer]);
+
+    const handleAnswer = (selectedOption) => {
+        setSelectedAnswer(selectedOption);
+
+        const question = questions[currentQuestion];
+        if (selectedOption === question.correct_answer) {
+            setScore((prevScore) => prevScore + 1);
+        }
+
+        setTimeout(() => {
+            setSelectedAnswer(null);
+            if (currentQuestion + 1 < questions.length) {
+                setCurrentQuestion((prevQuestion) => prevQuestion + 1);
+                setTimer(10);
+            }
+        }, 1000);
+    };
+
+    const handlePrevious = () => {
+        if (currentQuestion > 0) {
+            setCurrentQuestion((prevQuestion) => prevQuestion - 1);
+            setTimer(10);
+        }
+    };
+
+    const renderResult = () => {
+        return (
+            <div>
+                <h2>Quiz Completed!</h2>
+                <p>Your Score: {score}/{questions.length}</p>
+            </div>
+        );
+    };
+
+    const renderQuestion = () => {
+        const question = questions[currentQuestion];
+        const options = [...question.incorrect_answers, question.correct_answer];
+        shuffleArray(options);
+
+        return (
+            <div>
+                <h2>{question.question}</h2>
+                <div>
+                    {options.map((option) => (
+                        <button
+                            key={option}
+                            onClick={() => handleAnswer(option)}
+                            disabled={selectedAnswer !== null}
+                            className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2 ${selectedAnswer === option ? 'bg-gray-700' : ''
+                                }`}
+                        >
+                            {option}
+                        </button>
+                    ))}
+                </div>
+                {currentQuestion > 0 && (
+                    <button
+                        onClick={handlePrevious}
+                        className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded m-2"
+                    >
+                        Previous
+                    </button>
+                )}
+                <p>Time Remaining: {timer}</p>
+            </div>
+        );
+    };
+
+    if (questions.length === 0) {
+        return <div>Loading...</div>;
+    }
+
+    if (currentQuestion < questions.length) {
+        return (
+            <div className="container mx-auto p-4">
+                {renderQuestion()}
+            </div>
+        );
+    } else {
+        return (
+            <div className="container mx-auto p-4">
+                {renderResult()}
+            </div>
+        );
+    }
+};
+
+// Utility function to shuffle array elements
+const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+};
+
+export default Quiz;
+*/
