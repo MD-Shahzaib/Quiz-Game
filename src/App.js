01e0248b311f1c2e5b0439cc401de82fd3359195
Muhammad Shahzaib <mdshahzaib36@gmail.com>
2023-07-20 13:50:16 -0700

    Render Quiz Questions

--- src/App.js
+++ src/App.js
@@ -5,103 +5,129 @@ import Footer from './components/Footer';
 
 function App() {
   const [questions, setQuestions] = useState([]);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [timer, setTimer] = useState(300);
+  const [score, setScore] = useState(0);
+  const [showResult, setShowResult] = useState(false);
+
+  // Grab Current Quiz.
+  const presentQuiz = questions[currentQuestion];
+  const presentid = presentQuiz?.id;
+  const presentQuestion = presentQuiz?.question;
+  const presentOptions = presentQuiz?.options;
+  const presentanswer = presentQuiz?.answer;
+
+  // Fetch Questions.
   useEffect(() => {
     setQuestions(Questions)
   }, [])
 
-  // TIMER FUNCTION.
-  const [timer, setTimer] = useState(1);
+  // Timer Function.
   useEffect(() => {
     const interval = setInterval(() => {
-      setTimer((prevTimer) => prevTimer + 1);
+      setTimer((prevTimer) => prevTimer - 1);
     }, 1000);
     return () => { clearInterval(interval) };
   }, []);
 
-  // CLOCK FUNCTION FOR (Vanila-JS)
-  /*
-  function startClock(displayElement) {
-    function updateClock() {
-      const now = new Date();
-      const hours = now.getHours().toString().padStart(2, '0');
-      const minutes = now.getMinutes().toString().padStart(2, '0');
-      const seconds = now.getSeconds().toString().padStart(2, '0');
-      const time = `${hours}:${minutes}:${seconds}`;
-      displayElement.textContent = time;
+  // Handle Next Quiz.
+  const handleNextQuiz = () => {
+    if (parseInt(presentid) + 1 < questions.length) {
+      setCurrentQuestion((prevQuestion) => prevQuestion + 1);
     }
-    // Update the clock immediately
-    updateClock();
-    // Update the clock every second
-    setInterval(updateClock, 1000);
   }
-  // Example usage:
-  const displayElement = document.getElementById("clock"); // Element to display the clock
-  startClock(displayElement);
-  */
 
-  // CLOCK FUNCTION FOR (REACT)
-  /*
-  const [time, setTime] = useState('');
-  useEffect(() => {
-    const updateClock = () => {
-      const now = new Date();
-      const hours = now.getHours().toString().padStart(2, '0');
-      const minutes = now.getMinutes().toString().padStart(2, '0');
-      const seconds = now.getSeconds().toString().padStart(2, '0');
-      const currentTime = `${hours}:${minutes}:${seconds}`;
-      setTime(currentTime);
-    };
-    // Update the clock immediately
-    updateClock();
-    // Update the clock every second
-    const intervalId = setInterval(updateClock, 1000);
-    // Clean up the interval on component unmount
-    return () => clearInterval(intervalId);
-  }, []);
-  */
+  // Handle Previous Quiz.
+  const handlePreviousQuiz = () => {
+    if (parseInt(presentid) - 1 >= 0) {
+      setCurrentQuestion((prevQuestion) => prevQuestion - 1);
+    }
+  };
+
+  // Handle Answer.
+  // const handleAnswer = (selectedOption) => {
+  //   if (selectedOption === questions[currentQuestion].correctAnswer) {
+  //     setScore((prevScore) => prevScore + 1);
+  //   }
+
+  //   if (currentQuestion + 1 < questions.length) {
+  //     setCurrentQuestion((prevQuestion) => prevQuestion + 1);
+  //     setTimer(10);
+  //   } else {
+  //     setShowResult(true);
+  //   }
+  // };
+
+  // Handle Restart Quiz.
+  const handleRestartQuiz = () => {
+    alert("Restart Quiz")
+  }
 
   return (
     <>
       <Navbar />
       <div className="bg-slate-600">
-        <div className="container mx-auto p-4">
-          <h1 className='text-gray-200 text-4xl mb-4'>JavaScript Quiz</h1>
-          <h3 className='text-gray-200 text-xl mb-4 flex justify-between items-center'>
-            <span>Question 1 of {questions.length}</span>
-            <span>Time remaining: {timer} seconds</span>
-          </h3>
-          {questions?.map((item) => {
-            return (
-              <div key={item.id} className='bg-slate-200 p-2 my-2 rounded-md'>
-                <h2 className='text-2xl' >{parseInt(item.id) + 1} - {item.question}</h2>
-                <ul className='my-5'>
-                  {item.options.map((opt, index) => {
-                    return (
-                      <label
-                        key={index}
-                        htmlFor={opt}
-                        className="flex items-center space-x-2 p-2 bg-slate-700 text-white my-2 rounded-xl"
-                      >
-                        <input
-                          type="radio"
-                          name="option"
-                          id={opt}
-                          value={opt}
-                          className="form-radio h-5 w-5 text-blue-500"
-                        />
-                        <span>{opt}</span>
-                      </label>
-                    )
-                  })}
-                </ul>
-                <div className="flex justify-between items-center">
-                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">&larr; Previous</button>
-                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Next &rarr;</button>
-                </div>
+
+        {showResult ? (
+
+          <div className='container mx-auto p-4'>
+            {/* Result-Box */}
+            <div className='bg-slate-200 p-5 my-10 rounded-md flex flex-col justify-center items-center'>
+              <h1 className="text-4xl text-center font-semibold">Quiz Complete!</h1>
+              <p className="text-2xl my-4">Your score: {score}/{questions.length}</p>
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleRestartQuiz}>Restart Quiz &#8635;</button>
+            </div>
+            {/* Result-Box */}
+          </div>
+
+        ) : (
+
+          <div className="container mx-auto p-4">
+            <h1 className='text-gray-200 text-4xl mb-4'>JavaScript Quiz</h1>
+            <h3 className='text-gray-200 text-xl mb-4 flex justify-between items-center'>
+              <span>Question {parseInt(presentid) + 1} of {questions.length}</span>
+              <span>Time remaining: {timer} seconds</span>
+            </h3>
+            {/* Question-Box */}
+            <div className='bg-slate-200 p-2 my-10 rounded-md'>
+              <h2 className='text-2xl'>{parseInt(presentid) + 1} - {presentQuestion}</h2>
+              <div className='my-5'>
+                {presentOptions?.map((opt, index) => {
+                  return (
+                    <label key={index} htmlFor={opt} className="flex items-center space-x-2 p-2 bg-slate-700 text-white my-2 rounded-xl">
+                      <input
+                        type="radio"
+                        name="option"
+                        id={opt}
+                        value={opt}
+                        className="form-radio h-5 w-5 text-blue-500"
+                      />
+                      <span>{opt}</span>
+                    </label>
+                  )
+                })}
               </div>
-            )
-          })}
-        </div>
+              <div className="flex justify-between items-center">
+                <button
+                  disabled={parseInt(presentid) <= 0}
+                  onClick={handlePreviousQuiz}
+                  className="bg-blue-500 hover:bg-blue-700 disabled:bg-blue-300 text-white font-bold py-2 px-4 rounded"
+                >
+                  &larr; Previous
+                </button>
+                <button
+                  onClick={handleNextQuiz}
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                >
+                  {parseInt(presentid) + 1 !== questions.length ? "Next" : "Submit"} &rarr;
+                </button>
+              </div>
+            </div>
+            {/* Question-Box */}
+          </div>
+
+        )}
+
       </div>
       <Footer />
     </>
--- src/components/ExtraCode.js
+++ src/components/ExtraCode.js
@@ -331,3 +331,372 @@ const shuffleArray = (array) => {
 
 export default Quiz;
 */
+
+
+
+// __________________________________________________________________________.
+
+
+// Component/Question.js
+/* 
+import React from 'react';
+function Question({ question }) {
+return (
+  <div><h2 className="text-2xl font-bold mb-4">{question.question}</h2></div>
+ );
+}
+export default Question;
+*/ 
+
+
+// Component/Options.js
+/*
+import React from 'react';
+function Options({ options, handleAnswer }) {
+  return (
+    <div>
+      {options.map((option, index) => (
+        <div key={index}>
+          <label>
+            <input
+              type="radio"
+              name="option"
+              value={index}
+              onChange={handleAnswer}
+            />
+            {option}
+          </label>
+        </div>
+      ))}
+    </div>
+  );
+}
+export default Options;
+*/
+
+
+// Component/Result.js
+/*
+import React from 'react';
+function Result({ score, restartQuiz }) {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">Quiz Result</h2>
+      <p>Your score: {score}</p>
+      <button
+        onClick={restartQuiz}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-4 rounded"
+      >
+        Restart Quiz
+      </button>
+    </div>
+  );
+}
+export default Result;
+*/
+
+
+// Component/Quiz.js
+/*
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+import Question from './Question';
+import Options from './Options';
+import Result from './Result';
+
+function Quiz() {
+  const history = useHistory();
+  const [questions, setQuestions] = useState([]);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    axios
+      .get('/data/questions.json')
+      .then((response) => {
+        setQuestions(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching questions:', error);
+      });
+  }, []);
+
+  const handleAnswer = (event) => {
+    const selectedOption = parseInt(event.target.value);
+    const currentScore =
+      selectedOption === questions[currentQuestion].answer ? 1 : 0;
+
+    setScore(score + currentScore);
+    setCurrentQuestion(currentQuestion + 1);
+  };
+
+  const restartQuiz = () => {
+    setCurrentQuestion(0);
+    setScore(0);
+  };
+
+  if (currentQuestion >= questions.length) {
+    return <Result score={score} restartQuiz={restartQuiz} />;
+  }
+
+  return (
+    <div>
+      <Question question={questions[currentQuestion]} />
+      <Options
+        options={questions[currentQuestion].options}
+        handleAnswer={handleAnswer}
+      />
+      <button
+        onClick={() => setCurrentQuestion(currentQuestion + 1)}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-4 rounded"
+      >
+        Next
+      </button>
+    </div>
+  );
+}
+export default Quiz;
+*/
+
+
+// App.js
+/*
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Quiz from './components/Quiz';
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Quiz} />
+      </Switch>
+    </Router>
+  );
+}
+export default App;
+*/
+
+
+
+
+
+
+
+// __________________________________________________________________________.
+
+
+
+
+
+/*
+import React, { useState, useEffect } from "react";
+
+const questions = [
+  {
+    id: 1,
+    question: "What is the correct syntax for referring to an external script called 'script.js'?",
+    options: [
+      { id: 1, answer: "<script src='script.js'>" },
+      { id: 2, answer: "<script href='script.js'>" },
+      { id: 3, answer: "<script ref='script.js'>" },
+      { id: 4, answer: "<script name='script.js'>" },
+    ],
+    correctAnswer: 1,
+  },
+  {
+    id: 2,
+    question: "Which of the following is NOT a valid JavaScript data type?",
+    options: [
+      { id: 1, answer: "String" },
+      { id: 2, answer: "Boolean" },
+      { id: 3, answer: "Float" },
+      { id: 4, answer: "Object" },
+    ],
+    correctAnswer: 3,
+  },
+  {
+    id: 3,
+    question: "What does the 'typeof' operator in JavaScript return?",
+    options: [
+      { id: 1, answer: "The type of a variable" },
+      { id: 2, answer: "The size of an array" },
+      { id: 3, answer: "The length of a string" },
+      { id: 4, answer: "The index of an element in an array" },
+    ],
+    correctAnswer: 1,
+  },
+  {
+    id: 4,
+    question: "Which keyword is used to declare a constant variable in JavaScript?",
+    options: [
+      { id: 1, answer: "let" },
+      { id: 2, answer: "var" },
+      { id: 3, answer: "const" },
+      { id: 4, answer: "constan" },
+    ],
+    correctAnswer: 3,
+  },
+  {
+    id: 5,
+    question: "What is the output of the following code?\n\nconsole.log(2 + '2');",
+    options: [
+      { id: 1, answer: "4" },
+      { id: 2, answer: "22" },
+      { id: 3, answer: "NaN" },
+      { id: 4, answer: "undefined" },
+    ],
+    correctAnswer: 2,
+  },
+  {
+    id: 6,
+    question: "Which method is used to add an element to the end of an array in JavaScript?",
+    options: [
+      { id: 1, answer: "push()" },
+      { id: 2, answer: "pop()" },
+      { id: 3, answer: "shift()" },
+      { id: 4, answer: "unshift()" },
+    ],
+    correctAnswer: 1,
+  },
+  {
+    id: 7,
+    question: "What does the '===' operator in JavaScript compare?",
+    options: [
+      { id: 1, answer: "Values and types" },
+      { id: 2, answer: "Values only" },
+      { id: 3, answer: "Types only" },
+      { id: 4, answer: "None of the above" },
+    ],
+    correctAnswer: 1,
+  },
+  {
+    id: 8,
+    question: "Which built-in method returns the index within the calling string object of the first occurrence of the specified value?",
+    options: [
+      { id: 1, answer: "indexOf()" },
+      { id: 2, answer: "search()" },
+      { id: 3, answer: "substr()" },
+      { id: 4, answer: "charAt()" },
+    ],
+    correctAnswer: 1,
+  },
+  {
+    id: 9,
+    question: "Which JavaScript function is used to round a number to the nearest integer?",
+    options: [
+      { id: 1, answer: "Math.ceil()" },
+      { id: 2, answer: "Math.floor()" },
+      { id: 3, answer: "Math.round()" },
+      { id: 4, answer: "Math.random()" },
+    ],
+    correctAnswer: 3,
+  },
+  {
+    id: 10,
+    question: "Which statement is used to exit a loop in JavaScript?",
+    options: [
+      { id: 1, answer: "break" },
+      { id: 2, answer: "continue" },
+      { id: 3, answer: "return" },
+      { id: 4, answer: "exit" },
+    ],
+    correctAnswer: 1,
+  },
+];
+
+const App = () => {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showResult, setShowResult] = useState(false);
+  const [timer, setTimer] = useState(10);
+
+  // Timer Function.
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimer((prevTimer) => prevTimer - 1);
+    }, 1000);
+    return () => { clearInterval(interval) };
+  }, []);
+
+  const handleAnswer = (selectedOption) => {
+    if (selectedOption === questions[currentQuestion].correctAnswer) {
+      setScore((prevScore) => prevScore + 1);
+    }
+
+    if (currentQuestion + 1 < questions.length) {
+      setCurrentQuestion((prevQuestion) => prevQuestion + 1);
+      setTimer(10);
+    } else {
+      setShowResult(true);
+    }
+  };
+
+  const handleNextQuestion = () => {
+    if (currentQuestion + 1 < questions.length) {
+      setCurrentQuestion((prevQuestion) => prevQuestion + 1);
+      setTimer(10);
+    }
+  };
+
+  const handlePreviousQuestion = () => {
+    if (currentQuestion - 1 >= 0) {
+      setCurrentQuestion((prevQuestion) => prevQuestion - 1);
+      setTimer(10);
+    }
+  };
+
+  const handleRestartQuiz = () => {
+    setCurrentQuestion(0);
+    setScore(0);
+    setShowResult(false);
+    setTimer(10);
+  };
+
+  const renderOptions = () => {
+    return questions[currentQuestion].options.map((option) => (
+      <button
+        key={option.id}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-2 rounded"
+        onClick={() => handleAnswer(option.id)}
+      >
+        {option.answer}
+      </button>
+    ));
+  };
+
+  return (
+    <div className="container mx-auto mt-8 px-4">
+      {showResult ? (
+        <div>
+          <h1 className="text-4xl font-bold mb-4">Quiz Complete!</h1>
+          <p className="text-2xl mb-2">Your score: {score}/{questions.length}</p>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleRestartQuiz}>Restart Quiz</button>
+        </div>
+      ) : (
+        <div>
+          <h1 className="text-4xl font-bold mb-4">Question {currentQuestion + 1}/{questions.length}</h1>
+          <p className="text-2xl mb-2">{questions[currentQuestion].question}</p>
+          <div className="mb-4">{renderOptions()}</div>
+          <div className="flex justify-between">
+            {currentQuestion !== 0 && (
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handlePreviousQuestion}>Previous</button>
+            )}
+            <p className="text-xl">Time left: {timer}s</p>
+            {currentQuestion !== questions.length - 1 && (
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleNextQuestion}>Next</button>
+            )}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
+*/
+
+
+
+
+
